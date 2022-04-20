0fc981b80449f033c7f2400ec7e58265cb0e85ef
Caylem <caylemh@gmail.com>
2022-04-20 11:24:38 +0200

    Challenge 8 - Update code for 'compose' page to publish something to console.

--- app.js
+++ app.js
@@ -41,6 +41,13 @@ app.get("/compose", function(req,res){
   res.render("compose");
 });
 
+// Post compose route
+app.post("/compose", function(req,res){
+  let post = req.body.viewComposePost;
+  
+  console.log(post);
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
