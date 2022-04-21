3ff525bc82460111fc8aaab95751ed84f5f5d223
Caylem <caylemh@gmail.com>
2022-04-21 11:56:41 +0200

    Challenge 18 - Implementing 'lodash' to sort out string errors and formatting.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash/string");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -61,26 +62,18 @@ app.post("/compose", function(req,res){
 
 //Adding route for each post
 app.get("/posts/:postName", function(req, res){
-<<<<<<< HEAD
-  const requestedTitle = req.params.postName;
+  const requestedTitle = _.lowerCase(req.params.postName);
 
   posts.forEach(function(post){
-    const storedTitle = post.postTitle;
+    const storedTitle = _.lowerCase(post.postTitle);
 
     if(storedTitle === requestedTitle){
-      console.log("Match Found!");
-=======
-
-  posts.forEach(function(post){
-    if(post.postTitle === req.params.postName){
-      console.log("Match Found!");
-      res.redirect("/");
+      console.log("Match Found!" + `storedTitle = ${storedTitle} & requestedTitle = ${requestedTitle}`);
     } else {
-      res.redirect("/");
->>>>>>> e2ad487189c34ddbe417d2a32c63859a1657fc4b
+      console.log("No Match Found!" + `storedTitle = ${storedTitle} & requestedTitle = ${requestedTitle}`);
     }
   });
-
+  console.log("\n");
 });
 
 
